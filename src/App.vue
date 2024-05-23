e32bec9fbc9a68e0b3cb859421c39da5b309144a
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-23 13:26:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script >
+
+</script>
+
+<template>
+  <h1>Funzioni?</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique soluta accusamus minus cum repudiandae, ex, illum fugiat autem, repellat necessitatibus culpa nesciunt tempore eius alias. Doloribus quibusdam quas assumenda totam?</p>
+</template>
+
+<style >
+
+</style>
